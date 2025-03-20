400e0f4953d4dc42bba8b03376e6ac9c5c1108cc
Ryan Figueredo <ryan.figueredo059@gmail.com>
2025-03-20 17:34:14 -0300

    feat: add @vercel/analytics dependency and integrate analytics component in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Rasa } from "next/font/google";
 import "./styles.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const rasa = Rasa({ subsets: ["latin"] });
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={rasa.className}>{children}</body>
+      <body className={rasa.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
